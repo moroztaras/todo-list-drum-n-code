d6b940347324429f49393af58f2321961195229d
tarasmoroz <taras.moroz@ekreative.com>
2023-10-02 16:13:46 +0200

    Added list tasks sort by field of user

--- src/Controller/Api/TaskController.php
+++ src/Controller/Api/TaskController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use App\Manager\TaskManager;
-use App\Response\SuccessResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('api/task', name: 'api_twitter')]
+#[Route('api/task', name: 'api_task')]
 class TaskController extends ApiController
 {
     public function __construct(
@@ -14,4 +17,16 @@ class TaskController extends ApiController
     ) {
     }
 
+    // List tasks sort by field of user
+    #[Route('', name: 'api_task_list', methods: 'GET')]
+    public function list(Request $request): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getCurrentUser($request);
+        $sortBy = $request->query->get('sort_by', 'createdAt');
+
+        return $this->json([
+            'tasks' => $this->taskManager->getTasksOfUser($user->getId(), (string) $sortBy),
+        ], Response::HTTP_OK);
+    }
 }
--- src/Manager/TaskManager.php
+++ src/Manager/TaskManager.php
@@ -2,17 +2,23 @@
 
 namespace App\Manager;
 
+use App\Entity\User;
 use App\Repository\TaskRepository;
 use App\Validator\Helper\ApiObjectValidator;
 use Doctrine\Persistence\ManagerRegistry;
 
 class TaskManager
 {
-
     public function __construct(
         private ManagerRegistry $doctrine,
         private ApiObjectValidator $apiObjectValidator,
         private TaskRepository $taskRepository,
     ) {
+    }
+
+    public function getTasksOfUser(int $userId, string $sortBy)
+    {
+        return $this->taskRepository->getTasksOfUserAndSortByField($userId, $sortBy);
+
     }
 }
